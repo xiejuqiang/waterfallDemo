f7a321bab45b685284121387033e9f264a4fa05a
apple <apple@appletekiMac-mini.local>
2013-09-05 14:11:50 +0800

    Initial Commit

---
+++ waterfallDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  waterfallDemo
+//
+//  Created by apple on 13-9-5.
+//  Copyright (c) 2013年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
